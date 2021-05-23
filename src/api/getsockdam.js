0752c9312e3dc5269d77d8015623202b163e8c0d
JaewanJeong <jwmsg0525@gmail.com>
2021-05-23 16:32:13 +0900

    fix

--- src/api/getsockdam.js
+++ src/api/getsockdam.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-axios.defaults.baseURL = '';
+axios.defaults.baseURL = 'http://api.jwmsg.kr/';
 
 export const getMessage = async ()=>{
     return await axios.get("/api/sockdam").then(res=>res.data)
